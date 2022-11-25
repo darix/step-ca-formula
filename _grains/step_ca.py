7b2f584b4d40bd2d25bf013a4ef6c061a8473e20
Marcus Rückert <darix@nordisch.org>
2022-11-25 15:22:27 +0100

    Add custom grain to get the ssh host pubkeys

---
+++ _grains/step_ca.py
@@ -0,0 +1,17 @@
+import salt.utils
+import glob
+import os.path
+import re
+
+def ssh_pub_keys():
+    values = {}
+
+    for filename in glob.glob('/etc/ssh/ssh_host_*_key.pub'):
+      match = re.search('/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub', filename)
+      if os.path.exists(filename):
+          with open(filename, 'r') as f:
+              values[match.group('key_type')] = f.read()
+
+    ret = { 'ssh': { 'hostkeys': { 'pubkeys': values } } }
+
+    return ret
