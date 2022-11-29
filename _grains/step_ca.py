0b97d28af84ebccd0a0ccb049add4f4c2bf3d4a5
Daniel Mach <daniel.mach@suse.com>
2022-11-29 11:31:51 +0100

    Use r-string

--- _grains/step_ca.py
+++ _grains/step_ca.py
@@ -8,7 +8,7 @@ def ssh_pub_keys():
     values = {}
 
     for filename in glob.glob("/etc/ssh/ssh_host_*_key.pub"):
-        match = re.search("/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", filename)
+        match = re.search(r"/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", filename)
         # plain "dsa' is not supported by step-ca
         if "dsa" != match.group("key_type") and os.path.exists(filename):
             with open(filename, "r") as f:
