5db0ee7a5baf72ba7a6117bc70d7c31e9bcc099d
Daniel Mach <daniel.mach@suse.com>
2022-11-29 11:35:55 +0100

    Remove path.exists() condition, the file exists because it was returned from glob()

--- _grains/step_ca.py
+++ _grains/step_ca.py
@@ -10,7 +10,7 @@ def ssh_pub_keys():
     for path in glob.glob("/etc/ssh/ssh_host_*_key.pub"):
         match = re.search(r"/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", path)
         # plain "dsa" is not supported by step-ca
-        if "dsa" != match.group("key_type") and os.path.exists(path):
+        if "dsa" != match.group("key_type"):
             with open(path, "r") as f:
                 values[match.group("key_type")] = f.read()
 
