b0ab457a70a9472ae2ace1bfb4b4c793a59c560c
Daniel Mach <daniel.mach@suse.com>
2022-11-29 11:34:47 +0100

    Replace 'filename' with 'path'

--- _grains/step_ca.py
+++ _grains/step_ca.py
@@ -7,11 +7,11 @@ import re
 def ssh_pub_keys():
     values = {}
 
-    for filename in glob.glob("/etc/ssh/ssh_host_*_key.pub"):
-        match = re.search(r"/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", filename)
+    for path in glob.glob("/etc/ssh/ssh_host_*_key.pub"):
+        match = re.search(r"/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", path)
         # plain "dsa" is not supported by step-ca
-        if "dsa" != match.group("key_type") and os.path.exists(filename):
-            with open(filename, "r") as f:
+        if "dsa" != match.group("key_type") and os.path.exists(path):
+            with open(path, "r") as f:
                 values[match.group("key_type")] = f.read()
 
     ret = {"ssh": {"hostkeys": {"pubkeys": values}}}
