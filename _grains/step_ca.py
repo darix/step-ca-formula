8bb911708713a5a21e25050562802617564b7129
Daniel Mach <daniel.mach@suse.com>
2022-11-29 11:14:11 +0100

    Apply 'black' tool

--- _grains/step_ca.py
+++ _grains/step_ca.py
@@ -3,16 +3,17 @@ import glob
 import os.path
 import re
 
+
 def ssh_pub_keys():
     values = {}
 
-    for filename in glob.glob('/etc/ssh/ssh_host_*_key.pub'):
-        match = re.search('/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub', filename)
+    for filename in glob.glob("/etc/ssh/ssh_host_*_key.pub"):
+        match = re.search("/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", filename)
         # plain "dsa' is not supported by step-ca
-        if 'dsa' != match.group('key_type') and os.path.exists(filename):
-            with open(filename, 'r') as f:
-                values[match.group('key_type')] = f.read()
+        if "dsa" != match.group("key_type") and os.path.exists(filename):
+            with open(filename, "r") as f:
+                values[match.group("key_type")] = f.read()
 
-    ret = { 'ssh': { 'hostkeys': { 'pubkeys': values } } }
+    ret = {"ssh": {"hostkeys": {"pubkeys": values}}}
 
     return ret
--- _modules/step_ca.py
+++ _modules/step_ca.py
@@ -20,30 +20,35 @@ import json
 
 log = logging.getLogger(__name__)
 
-def patch_provisioner_config( needle, config):
-  pathname='/var/lib/step-ca/.step/config/ca.json'
 
-  log.error("New config: {config}".format(config=config))
-  json_string=''
+def patch_provisioner_config(needle, config):
+    pathname = "/var/lib/step-ca/.step/config/ca.json"
 
-  with open(pathname, 'r+') as open_file:
-    parsed_config=json.load(open_file)
+    log.error("New config: {config}".format(config=config))
+    json_string = ""
 
-    provisioners= parsed_config['authority']['provisioners']
+    with open(pathname, "r+") as open_file:
+        parsed_config = json.load(open_file)
 
-    for provisioner in provisioners:
-      if provisioner['name'] == needle:
-        changed_settings=[]
-        for option, value in config.items():
-            log.error("Setting {option} for {value}".format(option=option, value=value))
-            if provisioner[option]!=value:
-              changed_settings.append(option)
-              provisioner[option]=value
-        json_string=json.dumps(parsed_config, indent=4)
-        open_file.seek(0)
-        open_file.truncate()
-        open_file.write(json_string)
-  if len(changed_settings) > 0:
-    return {'Applied settings': changed_settings}
+        provisioners = parsed_config["authority"]["provisioners"]
 
-  return None
+        for provisioner in provisioners:
+            if provisioner["name"] == needle:
+                changed_settings = []
+                for option, value in config.items():
+                    log.error(
+                        "Setting {option} for {value}".format(
+                            option=option, value=value
+                        )
+                    )
+                    if provisioner[option] != value:
+                        changed_settings.append(option)
+                        provisioner[option] = value
+                json_string = json.dumps(parsed_config, indent=4)
+                open_file.seek(0)
+                open_file.truncate()
+                open_file.write(json_string)
+    if len(changed_settings) > 0:
+        return {"Applied settings": changed_settings}
+
+    return None
