ab8f7d9fdce71624f2812a89f614f26ee9302724
Daniel Mach <daniel.mach@suse.com>
2022-11-29 11:33:55 +0100

    Fix typo

--- _grains/step_ca.py
+++ _grains/step_ca.py
@@ -9,7 +9,7 @@ def ssh_pub_keys():
 
     for filename in glob.glob("/etc/ssh/ssh_host_*_key.pub"):
         match = re.search(r"/etc/ssh/ssh_host_(?P<key_type>\S+)_key.pub", filename)
-        # plain "dsa' is not supported by step-ca
+        # plain "dsa" is not supported by step-ca
         if "dsa" != match.group("key_type") and os.path.exists(filename):
             with open(filename, "r") as f:
                 values[match.group("key_type")] = f.read()
